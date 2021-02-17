1661a0df83587d4c71526a8efc3867dac98ddc05
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-02-17 22:16:11 +0100

    first commt

---
+++ bank.php
@@ -0,0 +1,11 @@
+<?php
+/*
+  Plugin Name: Banking Plugin
+  Description: Custom registration and fund transfer form using shortcode and script as well
+  Version: 1.x
+  Author: Daniel Mabadeje
+ 
+ */
+
+
+ 
