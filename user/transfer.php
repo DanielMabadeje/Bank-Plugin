3803552f35db538f3ed67a66c7de2635799ae29b
DanielMabadeje <mabadejedaniel1@gmail.com>
2021-02-20 16:07:01 +0100

    adding styles and scripts for transactions

--- user/transfer.php
+++ user/transfer.php
@@ -32,6 +32,7 @@ function wordpress_custom_transfer_form($wallet, $account_name, $account_no, $am
     <?php
 
 }
+
 function wp_transfer_form_valid( $amount, $swift_code, $imf_code, $cto_code, $account_no, $account_name)  {
     global $customize_error_validation;
     $customize_error_validation = new WP_Error;
@@ -166,3 +167,38 @@ function wp_custom_shortcode_transfer() {
 //     }
 //     return $errors;
 // }
+
+
+
+//Styles and Scripts
+
+
+function register_transfer_plugin_scripts() {
+    
+        wp_register_style( 'transfer-css-plugin', plugins_url( '/bank/css/transaction.css' ) );
+        wp_register_style( 'bootstrap-plugin', 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css' );
+    
+        wp_register_script( 'script-plugin', plugins_url( 'bank/js/transaction.js' ) );
+    
+}
+    
+add_action( 'admin_enqueue_scripts', 'register_transfer_plugin_scripts' );
+    
+function load_transfer_plugin_scripts( $hook ) {
+    
+        // Load only on ?page=sample-page
+        // if( $hook != 'toplevel_page_get-user' ) {
+    
+        //     return;
+    
+        // }
+
+        // Load style & scripts.
+        wp_enqueue_style( 'getUser-css-plugin' );
+        wp_enqueue_style( 'bootstrap-plugin' );
+    
+        wp_enqueue_script( 'my-plugin' );
+    
+}
+    
+add_action( 'admin_enqueue_scripts', 'load_transfer_plugin_scripts' );
